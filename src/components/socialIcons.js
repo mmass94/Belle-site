6b7446617cc073f1358e7f2f30f76d9215ed162e
mmass94 <ce.m.mas.94@gmail.com>
2023-03-13 01:23:09 +0300

    Adding Social Accounts

--- src/components/socialIcons.js
+++ src/components/socialIcons.js
@@ -11,22 +11,35 @@ import {
 const SocialIcons = () => {
   return (
     <div class="social-icons d-flex justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-start">
-      <a href="https://wa.me/1234567890">
+      <a href="https://wa.me/+966552126699" target="_blank">
         <FontAwesomeIcon icon={faWhatsapp} color="#666" />
       </a>
-      <a href="https://www.snapchat.com/add/username" className="snapchat">
+      <a
+        href="https://www.snapchat.com/add/belle_saudi?share_id=CnbdeHlvAKE&locale=en-US"
+        className="snapchat"
+        target="_blank"
+      >
         <FontAwesomeIcon icon={faSnapchat} color="#666" />
       </a>
-      <a href="https://www.youtube.com/channel/channel_id">
+      <a href="https://www.youtube.com/@belle_saudi" target="_blank">
         <FontAwesomeIcon icon={faYoutube} color="#666" />
       </a>
-      <a href="https://twitter.com/username">
+      <a
+        href="https://twitter.com/belle_saudi?s=11&t=msld0ZcICLviFreOdBQH_A"
+        target="_blank"
+      >
         <FontAwesomeIcon icon={faTwitter} color="#666" />
       </a>
-      <a href="https://www.facebook.com/username">
+      <a
+        href="https://m.facebook.com/story.php?story_fbid=1617802041982752&substory_index=1617802041982752&id=100089267318886&mibextid=Nif5oz"
+        target="_blank"
+      >
         <FontAwesomeIcon icon={faFacebook} color="#666" />
       </a>
-      <a href="https://www.instagram.com/username/">
+      <a
+        href="https://instagram.com/belle_saudi?igshid=YmMyMTA2M2Y="
+        target="_blank"
+      >
         <FontAwesomeIcon icon={faInstagram} color="#666" />
       </a>
     </div>
