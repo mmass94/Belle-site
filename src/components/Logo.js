428f0c4ebd2c37ba79aa5e60a0d9861c4527e5fd
mmass94 <ce.m.mas.94@gmail.com>
2023-03-13 19:23:43 +0300

    ammending ration of footer and logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,10 +2,32 @@ import logo from "../../logo.png";
 import Image from "next/image";
 
 const Logo = () => {
+  const aspectRatio = 1.2;
+  const width = 1282;
+  const height = width / aspectRatio;
   return (
     <div className="text-center col-md-12">
-      <Image src={logo} alt="Logo" width={230} height={230} />
+      <Image src={logo} alt="Logo" width={width / 5} height={height / 5} />
     </div>
   );
 };
 export default Logo;
+
+// import Image from 'next/image'
+
+// function MyComponent() {
+//   const aspectRatio = 1.5 // Replace with the actual aspect ratio of the image
+//   const width = 300 // Replace with the desired width of the image
+//   const height = width / aspectRatio // Calculate the height based on the aspect ratio
+
+//   return (
+//     <div>
+//       <Image
+//         src="/path/to/image.jpg"
+//         alt="Image description"
+//         width={width}
+//         height={height}
+//       />
+//     </div>
+//   )
+// }
--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,13 @@ import { Container, Row, Col } from "react-bootstrap";
 import SocialIcons from "../components/socialIcons";
 
 const Footer = () => {
+  const aspectRatio_Left = 1.419;
+  const width_Left = 904;
+  const height_Left = width_Left / aspectRatio_Left;
+
+  const aspectRatio_Right = 1.43;
+  const width_Right = 911;
+  const height_Right = width_Right / aspectRatio_Right;
   return (
     <footer className="bg-light py-2">
       <Container fluid>
@@ -17,18 +24,18 @@ const Footer = () => {
           </Col>
           <Col md={4} className="d-flex justify-content-center">
             <Image
-              src="/righfooterimg.jpg"
+              src="/righfooterimg.png"
               alt="Logo 3"
-              width={250}
-              height={250}
+              width={width_Right / 3.5}
+              height={height_Right / 3.5}
             />
           </Col>
           <Col md={4} className="d-flex justify-content-center">
             <Image
-              src="/leftfooterimg.jpg"
+              src="/leftfooterimg.png"
               alt="Logo 2"
-              width={250}
-              height={250}
+              width={width_Left / 3.5}
+              height={height_Left / 3.5}
             />
           </Col>
 
