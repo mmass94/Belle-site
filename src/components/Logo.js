da96530f9bec18afefcf241b280706847a3e70ce
mmass94 <ce.m.mas.94@gmail.com>
2023-03-13 18:28:00 +0300

    change Logo and photo images

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,16 +1,11 @@
-import logo from "../../logo.jpg"
-import Image from 'next/image'
+import logo from "../../logo.png";
+import Image from "next/image";
 
 const Logo = () => {
-return(
-<div className="text-center col-md-12"  >
-<Image 
-src={logo}
-alt="Logo"
-width={230}
-height={230}
-/>                
-</div>
-)
-}
-export default Logo
+  return (
+    <div className="text-center col-md-12">
+      <Image src={logo} alt="Logo" width={230} height={230} />
+    </div>
+  );
+};
+export default Logo;
